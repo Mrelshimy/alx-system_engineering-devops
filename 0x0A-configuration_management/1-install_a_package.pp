06662201c7c0dd24362ee4b555cbd9ce7062fba9
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-23 18:09:28 +0200

    file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Puppet Manifest to install flask from pip3.
-
 package { 'flask':
-  ensure   => '2.1.0',  # Required version
-  provider => 'pip3',   # pip3 as provider
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
