4fd2d263b6dda3f1d883bb92d3c02cb68d57bf2f
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-25 21:51:18 +0200

    update

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,9 +10,14 @@ if __name__ == "__main__":
     id = sys.argv[1]
     url = f"https://jsonplaceholder.typicode.com/users/{id}"
     todo_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-    user_name = requests.get(url).json().get('name')
+    user_name = requests.get(url).json().get('username')
     user_id = requests.get(url).json().get('id')
     user_todos = requests.get(todo_url)
-    with open(f'new.csv', 'w') as file:
-        for task in user_todos.json():
-            json.dumps
+    task_list = []
+    for task in user_todos.json():
+        task_dict = {"task": task.get('title'),
+                     "completed": task.get('completed'), "username": user_name}
+        task_list.append(task_dict)
+    user_dict = {user_id: task_list}
+    with open(f'{user_id}.json', 'w') as file:
+        json.dump(user_dict, file)
