332cd320b529db1481bbad779bb7c49225cf48ae
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-25 19:48:05 +0200

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,13 +6,16 @@ import sys
 if __name__ == "__main__":
     """Request employee data and represent it"""
 
-    user_name = requests.get(f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}").json().get('name')
-    user_todos = requests.get(f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos")
-    finished_tasks=[]
+    id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
+    user_name = requests.get(url).json().get('name')
+    user_todos = requests.get(todo_url)
+    finished_tasks = []
     for task in user_todos.json():
-        if task.get('completed') == True :
+        if task.get('completed') is True:
             finished_tasks.append(task.get('title'))
-    print(f"Employee {user_name} is done with tasks({len(finished_tasks)}/{len(user_todos.json())}):")
+    print(f"Employee {user_name} is done with tasks\
+({len(finished_tasks)}/{len(user_todos.json())}):")
     for item in finished_tasks:
         print(f'\t{item}')
-    
