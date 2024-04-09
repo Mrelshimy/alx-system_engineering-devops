b511f7f38d841f7af2b6718f0fff363c5f01812a
Mrelshimy <mraafat.elsayed@gmail.com>
2024-04-09 22:53:38 +0200

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Function that  queries the Reddit API and
     returns the number of subscribers)"""
     link = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    responce = requests.get(link, headers={'User-Agent': 'app/1.0'})
+    responce = requests.get(link, headers={'User-Agent': 'raafat/1.0'})
     if responce.status_code == 200:
         data = responce.json()
         return(data['data']['subscribers'])
