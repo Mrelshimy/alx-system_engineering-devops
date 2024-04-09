644e78ebd5d6280fe21092176a00be5207244db7
Mrelshimy <mraafat.elsayed@gmail.com>
2024-04-09 23:07:59 +0200

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
-
-from requests import get
+"""module that queries the Reddit API and returns the number of subscribers"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
+    """Function that  queries the Reddit API and
+    returns the number of subscribers)"""
+    link = 'https://www.reddit.com/r/{}/about.json'.format(str(subreddit))
+    responce = requests.get(link, headers={'User-Agent': 'raafat'})
+    if responce.status_code == 200:
+        data = responce.json()
+        if data.get('data').get('subscribers') == None:
+            return 0
+        else:
+            return data.get('data').get('subscribers')
+    else:
         return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    
+result = number_of_subscribers('pytho')
+print(result)
