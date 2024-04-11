83af9b16feb34f424abb2f57447d2ee470d24693
Mrelshimy <mraafat.elsayed@gmail.com>
2024-04-11 16:33:06 +0200

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Fix 500 error when a GET HTTP method is requested to Apache web server
 
-exec {'replace':
-  provider => shell,
+exec {'strace':
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
